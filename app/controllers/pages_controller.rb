4e7ad6f48795308046b5f15bf08db84a1327327c
Jonny Gates <jonny.gates@outlook.com>
2019-08-01 12:03:05 +0100

    Embed assistant in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
+    if params[:user_input]
+      assistant = WatsonAssistant.new
+      @message = assistant.send_message(params[:user_input])
+    end
   end
 end
